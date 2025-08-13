3b135f075d54c3842244f1fd063887b82d663f9b
Kenton071 <kentontran07@gmail.com>
2025-08-13 02:23:35 -0400

    Create InitialsKT.java

---
+++ InitialsKT.java
@@ -0,0 +1,38 @@
+// Program Name: Kenton's Initials But Extremely Cool
+// Description: This program prints out Kenton Tran's initals in very cool way.
+// Date: 8/13/2025
+// Programmer: Kenton T.Tran 
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" ____   ____");
+        System.out.println(" |KK|  /KK/");
+        System.out.println(" |KK| /KK/");
+        System.out.println(" |KK|/KK/");
+        System.out.println(" |KKKKK/");
+        System.out.println(" |KK|\\KK\\");
+        System.out.println(" |KK| \\KK\\");
+        System.out.println(" |KK|  \\KK\\");
+        System.out.println(" ----   ----");
+
+        System.out.println(" ____________");
+        System.out.println(" |TTTTTTTTTT|");
+        System.out.println("     |TT|    ");
+        System.out.println("     |TT|    ");
+        System.out.println("     |TT|    ");
+        System.out.println("     |TT|    ");
+        System.out.println("     |TT|    ");
+        System.out.println("     |TT|    ");
+        System.out.println("     ----    ");
+
+        System.out.println(" ____________");
+        System.out.println(" |TTTTTTTTTT|");
+        System.out.println("     |TT|    ");
+        System.out.println("     |TT|    ");
+        System.out.println("     |TT|    ");
+        System.out.println("     |TT|    ");
+        System.out.println("     |TT|    ");
+        System.out.println("     |TT|    ");
+        System.out.println("     ----    ");
+    }
+}
